d4c0d066a4bf7b2b925e4f85b418119753ba5ff1
amitmilo <amit.milo.43@gmail.com>
2022-02-14 11:21:03 +0200

    Added supervector

---
+++ super_vector.py
@@ -0,0 +1,25 @@
+class SuperVector:
+    def __init__(self, left, right, connection):
+        self.left = left
+        self.right = right
+        self.connection = connection
+        self.connection_logic = None if connection is None else {"או": max, "וגם": min}[connection]
+
+    @staticmethod
+    def parse(tokens):
+        connections = ["או", "וגם"]
+
+        for i, token in enumerate(tokens):
+            if token in connections:
+                return SuperVector(SuperVector.parse(tokens[:i]), SuperVector.parse(tokens[i + 1:]), token)
+        return SuperVector(tokens, None, None)
+
+    def apply_sim_func(self, sim_func):
+        if self.connection is None:
+            return sim_func(self.left)
+        return self.connection_logic(self.left.apply_sim_func(sim_func), self.right.apply_sim_func(sim_func))
+
+    def __str__(self):
+        if self.right is None:
+            return ' '.join(self.left)
+        return ' '.join([str(self.left), self.connection, str(self.right)])
